927842cef7eea9dfbb8309de004bcd5b20fab0a7
Rówienicz Łukasz <p1565@PCOSA.AD>
2024-04-09 14:58:07 +0200

    Rdata

---
+++ R/DroneData.R
@@ -0,0 +1,11 @@
+#' Drone positions data
+#'
+#' The data set contains x,y,z position in global reference frame as function of time
+#'
+#' @docType data
+#' @usage data (DroneData)
+#' @format A data frame
+#' @keywords datasets DroneData
+#' @examples
+#' data (DroneData)
+#' plot (DroneData$t, DroneData$x)
